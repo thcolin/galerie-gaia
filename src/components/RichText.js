350c2e01a01bd790e9e53257c1b6f781bf19b080
thcolin <th.colin3@gmail.com>
2020-02-22 18:18:55 +0100

    Fix build

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -87,7 +87,7 @@ const RichText = ({ children, options = {}, ...props }) => (
 
 RichText.styles = {
   element: {
-    
+
   },
 }
 
--- src/utils/resolve.js
+++ src/utils/resolve.js
@@ -2,8 +2,8 @@ export function fromGatsby2Filesystem (input) {
   return `src/pages/${input}`
 }
 
-export function fromFilesystem2Gatsby (input, { extension = true }) {
-  return input.replace(/src\/pages\//, '').replace(extension ? '' : /\.md$/, '')
+export function fromFilesystem2Gatsby (input, options = { extension: true }) {
+  return input.replace(/src\/pages\//, '').replace(options.extension ? '' : /\.md$/, '')
 }
 
 export default {
