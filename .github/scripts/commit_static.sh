ee499f4d8b58bc841e02bfa57878d01690108499
thcolin <th.colin3@gmail.com>
2020-03-12 19:45:34 +0100

    Handle empty static changes on Github Actions

---
+++ .github/scripts/commit_static.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -z "$(git status --porcelain)" ]; then
+  # Working directory clean, nothing to commit
+else
+  git add static
+  git commit -am "Update from Gulp - Updated static folder"
+  git push
+fi
