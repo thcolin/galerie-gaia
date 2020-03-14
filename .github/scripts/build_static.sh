9c0524424d21577e3304817328e64acccfc9d6d7
thcolin <th.colin3@gmail.com>
2020-03-14 15:46:21 +0100

    Refactor gatsby deploy to gh-pages

--- .github/scripts/build_static.sh
+++ .github/scripts/build_static.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git config --global user.name 'Thomas Colin'
+git config --global user.email 'thcolin@users.noreply.github.com'
+npm install -g gulp
+npm install
 gulp
 
 if [ -z "$(git status --porcelain)" ]; then
--- .github/scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.name 'Thomas Colin'
-git config --global user.email 'thcolin@users.noreply.github.com'
-npm install -g gulp
-npm install
