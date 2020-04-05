dd4d35df8ce2995fd006a11fd3755f28f59cf403
thcolin <th.colin3@gmail.com>
2020-04-05 21:55:39 +0200

    Update Contextual.js

--- src/components/Contextual.js
+++ src/components/Contextual.js
@@ -31,7 +31,7 @@ const Contextual = ({ work, ...props }) => {
   const [open, setOpen] = useState(false)
   
   useEvent('keydown', (e) => !(e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) && setOpen(false), typeof document !== 'undefined' && document)
-  useEvent('scroll', () => setOpen(false), typeof window !== undefined && window)
+  useEvent('scroll', () => setOpen(false), typeof window !== 'undefined' && window)
 
   return (
     <div {...props}>
