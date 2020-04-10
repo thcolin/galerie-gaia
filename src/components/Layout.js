bcb7bda6c2778088b91a52c9ad4368cdb8efa48a
thcolin <th.colin3@gmail.com>
2020-04-10 23:26:27 +0200

    Fix viewport initial-scale meta tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ const Layout = ({ children, ...props }) => (
         ].filter(s => s).join(' - ')}
       </title>
       <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width, initialScale=1.0' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta name='google' content='notranslate' />
       <meta property="og:locale" content="fr_FR" />
       <meta property="og:site_name" content={props.pageContext.site.siteMetadata.title} />
