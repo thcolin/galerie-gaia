51c61eda330ba93f771e1b59763e3691146645c8
thcolin <th.colin3@gmail.com>
2020-06-12 16:22:19 +0200

    Add google analytics and tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,26 @@ module.exports = {
     },
     'gatsby-redirect-from',
     'gatsby-plugin-meta-redirect',
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MLBX757",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+        // // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",  
+        // Name of the event that is triggered on every Gatsby route change.
+        routeChangeEventName: "gatsby-route-change",
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+          head: true,
+          trackingId: 'google19d6c9c3c8a5f3be'
+      }
+    },
   ],
 }
