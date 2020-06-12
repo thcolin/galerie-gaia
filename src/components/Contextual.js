5fc8ae0a8e4923fa0d7890be0bee36aa9586ce76
thcolin <th.colin3@gmail.com>
2020-06-12 17:02:58 +0200

    Update Contextual.js

--- src/components/Contextual.js
+++ src/components/Contextual.js
@@ -12,14 +12,14 @@ const Contextual = ({ work, ...props }) => {
     small: {
       height: [1, 69],
       width: [1, 70],
-      ratio: 160,
-      margin: `${24 - (12 * ((work.dimensions.height - 60) / (180 - 60)))}% 0 60%`,
+      ratio: 100,
+      margin: `15% 0 70%`,
     },
     medium: {
       height: [70, 165],
       width: [1, 132],
       ratio: 170, // (width of the picture in cm)
-      margin: `${24 - (12 * ((work.dimensions.height - 60) / (180 - 60)))}% 0 60%`,
+      margin: `${24 - (12 * ((work.dimensions.height - 70) / (165 - 70)))}% 0 60%`,
     },
   }
 
