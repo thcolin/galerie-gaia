81ea96f7b8e30a434a2a6c8a2b4f80053a503a78
thcolin <th.colin3@gmail.com>
2020-08-27 17:35:41 +0200

    Fix Logo as Link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Logo = ({ title, caption, ...props }) => (
   <div css={Logo.styles.element}>
-    <p>{title}</p>
+    <Link to="/">{title}</Link>
     <small>{caption}</small>
   </div>
 )
 
 Logo.styles = {
   element: {
-    p: {
+    '>a': {
+      display: 'block',
       margin: 0,
       padding: 0,
       fontSize: '3.5em',
       lineHeight: 1,
       textAlign: 'left',
+      textDecoration: 'none',
       letterSpacing: '0.125em',
     },
-    small: {
+    '>small': {
       display: 'block',
       padding: '1em 0 0',
       fontSize: '1em',
