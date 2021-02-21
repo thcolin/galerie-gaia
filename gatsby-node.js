b860a228059254127608bd71215ee5e86a12785c
thcolin <th.colin3@gmail.com>
2021-02-21 23:46:45 +0100

    Remove Gulp processs for S3 + lambda

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,26 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-const gulpfile = require('./gulpfile.js')
-
-exports.onPreInit = async () => {
-  await new Promise(resolve => {
-    const stream = gulpfile.images()
-    stream.on('finish', () => resolve())
-  })
-
-  await new Promise(resolve => {
-    const stream = gulpfile.thumbnails()
-    stream.on('finish', () => resolve())
-  })
-
-  await new Promise(resolve => {
-    const stream = gulpfile.traces()
-    stream.on('finish', () => resolve())
-  })
-
-  await new Promise(resolve => {
-    const stream = gulpfile.wipe()
-    stream.on('finish', () => resolve())
-  })
-}
