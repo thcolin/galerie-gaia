b1afb736d9aa2cf3fc780637e9932f63ca987701
thcolin <thcolin@users.noreply.github.com>
2021-07-13 10:03:54 +0000

    deployed via Gatsby Publish Action 🎩 for 93436f19840ffa92248ea94a4ca217e5b9030449

---
+++ component---src-pages-404-js-cfeb0306c888dd811803.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgalerie_gaia=self.webpackChunkgalerie_gaia||[]).push([[883],{9616:function(e,a,n){"use strict";n.r(a);var u=n(7294),i=n(5444);a.default=function(){return(0,u.useEffect)((function(){(0,i.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-cfeb0306c888dd811803.js.map
